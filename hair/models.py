19d4657f96297f140d233665fb647ca68b757b7a
Shishir Kumar Regmi <shishirregmi65@gmail.com>
2022-04-29 11:50:47 -0400

    1st commit

--- hair/models.py
+++ hair/models.py
@@ -1,13 +1,6 @@
 from django.db import models
-from django.core.validators import FileExtensionValidator
 
 # Create your models here.
-
-
-class importantPDF(models.Model):
-    title = models.CharField(max_length=100)
-    pdf_file = models.FileField(upload_to='files/',
-                                validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
-    timeAdded=models.DateTimeField(auto_now_add=True)
-
-  
+class gallery_images(models.Model):
+    image = models.ImageField(null=True,blank=True)
+    description = models.TextField(null=True,blank=True)
