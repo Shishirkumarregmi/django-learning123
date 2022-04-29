890881038ecdd13f208aa84512d311adce975f5d
Shishirkumarregmi <68994454+Shishirkumarregmi@users.noreply.github.com>
2022-04-29 12:01:27 -0400

    updated field

--- hair/models.py
+++ hair/models.py
@@ -5,3 +5,4 @@ class gallery_images(models.Model):
     image = models.ImageField(null=True,blank=True)
     description = models.TextField(null=True,blank=True)
     text=models.CharField(max_length=50)
+    ashok=models.CharField(max_length=50)
